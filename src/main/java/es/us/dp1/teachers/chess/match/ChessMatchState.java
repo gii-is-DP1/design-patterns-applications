bfca18f05563e270aab90f7b2b6a1ab4dc6e641c
japarejo <japarejo@gmail.com>
2025-12-10 20:02:57 +0100

    Refactoring of ChessMatchState to assign the responsibility of moveType identificatnio to ChessBoard instead of the state

--- src/main/java/es/us/dp1/teachers/chess/match/ChessMatchState.java
+++ src/main/java/es/us/dp1/teachers/chess/match/ChessMatchState.java
@@ -33,60 +33,13 @@ public abstract class ChessMatchState extends BaseEntity {
             if (target==null || !target.getColor().equals(expectedTargetColor()))
                 throw new IllegalMoveException("You cannot move other player's pieces");
             else {
-                boolean castlingMove = isCastlingMove(target, fromX, fromY, toX, toY);
-                Piece destinationPiece = match.getBoard().validateDestination(target, toX, toY);
-                if (castlingMove && destinationPiece != null) {
-                    throw new IllegalMoveException("Castling destination must be empty");
-                }
-                target.getType().validateMove(target, destinationPiece, fromX, fromY, toX, toY);
-                match.getBoard().validatePathClear(target, fromX, fromY, toX, toY);
-                Command command = buildCommandForMove(target, destinationPiece, fromX, fromY, toX, toY);
+                Command command = match.getBoard().createCommandForMove(target, fromX, fromY, toX, toY);
                 match.executeCommand(command);
                 ChessMatchState nexState = nextState();
                 nexState.setMatch(match);
                 match.setState(nexState);
             }
         }
-    }
-
-    protected Command buildCommandForMove(Piece target, Piece destinationPiece, int fromX, int fromY, int toX, int toY) {
-        if (isCastlingMove(target, fromX, fromY, toX, toY)) {
-            return createCastlingCommand(target, fromX, fromY, toX, toY);
-        }
-        if (isPromotionMove(target, toY)) {
-            return new PromotionCommand(target,  fromX, fromY,  toX, toY, PieceType.QUEEN, destinationPiece);
-        }
-        if (destinationPiece != null) {
-            return new CaptureCommand(target, destinationPiece, fromX, fromY, toX, toY);
-        }
-        return new MovePieceCommand(target, fromX, fromY, toX, toY);
-    }
-
-    private boolean isPromotionMove(Piece target, int toY) {
-        if (target.getType() != PieceType.PAWN) {
-            return false;
-        }
-        return (target.getColor() == PieceColor.WHITE && toY == 8) || (target.getColor() == PieceColor.BLACK && toY == 1);
-    }
-
-    private boolean isCastlingMove(Piece target, int fromX, int fromY, int toX, int toY) {
-        return target.getType() == PieceType.KING && fromY == toY && Math.abs(fromX - toX) == 2;
-    }
-
-    private Command createCastlingCommand(Piece king, int fromX, int fromY, int toX, int toY) {
-        int rookFromX = toX > fromX ? 8 : 1;
-        Piece rook = match.getBoard().getPieceAt(rookFromX, fromY);
-        if (rook == null || rook.getType() != PieceType.ROOK || rook.getColor() != king.getColor()) {
-            throw new IllegalMoveException("Castling requires an unmoved rook on the same rank");
-        }
-        int rookToX = toX > fromX ? toX - 1 : toX + 1;
-        Piece rookDestination = match.getBoard().validateDestination(rook, rookToX, toY);
-        if (rookDestination != null) {
-            throw new IllegalMoveException("Castling squares must be empty");
-        }
-        match.getBoard().validatePathClear(rook, rookFromX, fromY, rookToX, toY);
-        MovePieceCommand rookMove = new MovePieceCommand(rook, rookFromX, fromY, rookToX, toY);
-        return new CastlingCommand(king, fromX, fromY, toX, toY, rookMove);
     }
 
     public abstract PieceColor expectedTargetColor();
